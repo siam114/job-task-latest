27e7de7df5661e2906b7619974e65baf3fefa3e9
siam114 <smsiamislam598@gmail.com>
2025-02-21 19:12:02 +0600

    added button and textbox component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Users from "./pages/Users";
 import Trash from "./pages/Trash";
 import TaskDetails from "./pages/TaskDetails";
 import Login from "./pages/Login";
-import Toaster from "sonner";
+import {Toaster} from "sonner";
 
 
 function Layout (){
@@ -38,17 +38,17 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Navigate to="/dashboard" />} />
-          <Route path="/dashboard >" element={<Dashboard />} />
-          <Route path="/tasks >" element={<Tasks />} />
-          <Route path="/completed/:status >" element={<Tasks />} />
-          <Route path="/in-progress/:status >" element={<Tasks />} />
-          <Route path="/todo/:status >" element={<Tasks />} />
-          <Route path="/team >" element={<Users />} />
-          <Route path="/trashed >" element={<Trash />} />
-          <Route path="/task/:id >" element={<TaskDetails />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/completed/:status" element={<Tasks />} />
+          <Route path="/in-progress/:status" element={<Tasks />} />
+          <Route path="/todo/:status" element={<Tasks />} />
+          <Route path="/team" element={<Users />} />
+          <Route path="/trashed" element={<Trash />} />
+          <Route path="/task/:id" element={<TaskDetails />} />
         </Route>
 
-        <Route path="/log-in >" element={<Login />} />
+        <Route path="/log-in" element={<Login />} />
       </Routes>
 
       <Toaster richColors />
